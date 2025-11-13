6ab29543c9bd4e09fe84afc8aee3386e21fed966
ArnaudM1983 <arnaudmeusy@hotmail.com>
2025-11-13 16:54:44 +0100

    create bombes-de-peinture page

---
+++ app/bombes-de-peinture/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Page = (props: Props) => {
+  return (
+    <div>
+        <h1>Bombes de peinture</h1>
+    </div>
+  )
+}
+
+export default Page
